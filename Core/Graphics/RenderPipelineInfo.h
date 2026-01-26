5f34e3ce15018509e47f7f90d9006a0c345c2d76
James Mitchell <v010000010101001101010011@gmail.com>
2026-01-26 14:19:19 -0500

    Update RenderPipelineInfo.h

--- Core/Graphics/RenderPipelineInfo.h
+++ Core/Graphics/RenderPipelineInfo.h
@@ -4,28 +4,20 @@ typedef unsigned int uint32_t;
 
 struct VkShaderModule_T;
 struct VkPipelineShaderStageCreateInfo;
-
 struct VkPipelineDynamicStateCreateInfo;
 typedef enum VkDynamicState;
-
 struct VkPipelineVertexInputStateCreateInfo;
-
 struct VkPipelineInputAssemblyStateCreateInfo;
-
 struct VkViewport;
 struct VkRect2D;
 struct VkPipelineViewportStateCreateInfo;
-
 struct VkPipelineRasterizationStateCreateInfo;
 struct VkPipelineMultisampleStateCreateInfo;
-
 struct VkPipelineColorBlendAttachmentState;
 struct VkPipelineColorBlendStateCreateInfo;
-
 struct VkDevice_T;
 struct VkPipelineLayout_T;
 struct VkRenderPass_T;
-
 struct VkPipeline_T;
 
 struct GraphicsPipelineCreateParams {
